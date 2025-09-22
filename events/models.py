49b5e6cd460993bde4d23b8f01f0786352eb444b
Nafis Mahamud Shahin <nafismahamudshahin@gmail.com>
2025-09-22 23:15:09 +0600

    create models with relationship

--- events/models.py
+++ events/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField()
+    description = models.TextField()
+
+class Event(models.Model):
+    name = models.CharField()
+    description = models.TextField()
+    date = models.DateField()
+    time = models.TimeField()
+    location = models.CharField()
+    category = models.ForeignKey(Category,on_delete=models.CASCADE,default=1)
+
+
+class Participant(models.Model):
+    name = models.CharField()
+    email = models.EmailField()
+    event = models.ManyToManyField(Event)
+
+
+    
---
+++ events/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+# import from views:
+from events.views import *
+urlpatterns = [
+    path('test/',test),
+]
