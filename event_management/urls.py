abca7de21d3233d5dbeba55038c97a263c1986e5
Nafis Mahamud Shahin <nafismahamudshahin@gmail.com>
2025-09-25 04:32:56 +0600

    show auto home pages

--- event_management/urls.py
+++ event_management/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from django.urls import path ,include
 from debug_toolbar.toolbar import debug_toolbar_urls
-
+from participants.views import home
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',home),
     path('',include('events.urls')),
     path('',include('participants.urls')),
     path('',include('dashboard.urls')),
